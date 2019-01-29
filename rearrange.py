a2f9aebc409701edf2aca05cf9e8de6401784abe
Drew McGowan <michael.mcgowan@students.makeschool.com>
2019-01-28 16:46:42 -0800

    played with rearrange

--- rearrange.py
+++ rearrange.py
@@ -3,13 +3,23 @@ import sys
 
 words = sys.argv[1:]
 neworder = []
-randintlist = []
+# randintlist = []
 
-while len(neworder) < len(words):
-    num =  random.randint(0,len(words)-1)
-    if len(randintlist) == 0:
-        neworder.append(words[num])
-    elif num not in randintlist:
-        neworder.append(words(num))
+# while len(neworder) < len(words):
+#     num =  random.randint(0,len(words)-1)
+#     if len(randintlist) == 0:
+#         neworder.append(words[num])
+#         randintlist.append(num)
+#     elif num not in randintlist:
+#         neworder.append(words[num])
+#         randintlist.append(num)
+#
+# print(*neworder)
+
+length=len(words)
+for i in range(length):
+    num = random.randint(0, len(words)-1)
+    neworder.append(words[num])
+    words.pop(num)
 
 print(*neworder)

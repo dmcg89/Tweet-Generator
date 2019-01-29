95e26a3995a472661cf27417d20be15d6da41e2b
Drew McGowan <michael.mcgowan@students.makeschool.com>
2019-01-29 15:09:16 -0800

    list of lists and lists of tuples working

--- histogram.py
+++ histogram.py
@@ -2,30 +2,48 @@ import string
 
 
 word_file = 'text1.txt'
-text =  open(word_file).read()
-print(text)
-
-# removes punctuation from text and sets all ensures all characters are lower case
-translator = str.maketrans('', '', string.punctuation)
-text = text.lower()
-wordsFromText = (text.translate(translator)).split()
 
 def unique_words():
     uniqueWordSet = set(wordsFromText)
     print(len(uniqueWordSet))
-    return len(uniqueWordSet)
+    return (uniqueWordSet)
 
 def frequency(selectWord):
     frequency = 0
     for word in wordsFromText:
         if word == selectWord:
             frequency += 1
-    print(frequency)
+    # print(frequency)
     return frequency
 
-# unique_words()
-# frequency('mystery')
-# frequency('one')
+def hist_list_of_lists(uniqueWordSet):
+    histListOfLists = []
+    for word in uniqueWordSet:
+        wordFrequency = frequency(word)
+        histEntry = [word, wordFrequency]
+        histListOfLists.append(histEntry)
+    print(histListOfLists)
+
+def hist_list_of_tuples(uniqueWordSet):
+    histListOfTuples = []
+    wordFrequencyList = []
+    for word in uniqueWordSet:
+        wordFrequency = frequency(word)
+        wordFrequencyList.append(wordFrequency)
+    histListOfTuples = zip(uniqueWordSet, wordFrequencyList)
+    for a, *b in histListOfTuples:
+      print(a, ' '.join(map(str, b)))
+
+
+
 
 if __name__ == '__main__':
-    pass
+    text =  open(word_file).read()
+
+    # removes punctuation from text and sets all ensures all characters are lower case
+    translator = str.maketrans('', '', string.punctuation)
+    text = text.lower()
+    wordsFromText = (text.translate(translator)).split()
+    uniqueWordSet = unique_words()
+    # hist_list_of_lists(uniqueWordSet)
+    hist_list_of_tuples(uniqueWordSet)
